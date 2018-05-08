7eb6548918b028d1c2dfe21adf823346c2a3e52e
Mark Manalo <mmanalo@dealersocket.com>
2018-05-08 16:47:14 -0700

    Sample code

---
+++ ReadOnlyAutoProperty/ReadOnlyAutoProperty/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ReadOnlyAutoProperty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var customer = new Customer("firstName", "lastName");
+            Console.WriteLine(customer.FirstName + " " + customer.LastName);
+            Console.ReadLine();
+        }
+    }
+
+    public class Customer
+    {
+        // Readonly properties
+        // Can only be set in the constructor
+        public string FirstName { get; }
+        public string LastName { get; }
+
+        public Customer(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        public void ChangeName(string newFirstName)
+        {   
+            // This generates an error cuz its readonly
+            //FirstName = newFirstName;
+        }
+    }
+}
