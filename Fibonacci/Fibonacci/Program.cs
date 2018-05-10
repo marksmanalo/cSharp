0bbd24b9d1beeadc84aa0b3cef55c3d7f5d2a256
mmanalo <marksmanalo@marks-mbp-2.attlocal.net>
2018-05-09 20:56:13 -0700

    fibonacci no recursion

---
+++ Fibonacci/Fibonacci/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+// Output the Fibonacci sequence non recursively
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            PrintFibonacci(10);
+        }
+
+        private static void PrintFibonacci(int length)
+        {
+            var prevResult = 0;
+            var currentResult = 0;
+            for (var i = 0; i < length; i++)
+            {
+                if (i == 0)
+                {
+                    currentResult = 0;
+                }
+                if (i == 1)
+                {
+                    currentResult = 1;    
+                }
+                else
+                {
+                    var result = currentResult + prevResult;
+                    prevResult = currentResult;
+                    currentResult = result;
+                }
+
+                Console.WriteLine(currentResult);
+            }
+        }
+    }
+}
