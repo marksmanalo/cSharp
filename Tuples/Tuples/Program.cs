68072cb7c89e91496de4bdfa9a3eb51a26e3cd6e
Mark Manalo <mmanalo@dealersocket.com>
2018-05-10 14:38:38 -0700

    first checkin of Tuples project

---
+++ Tuples/Tuples/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tuples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
