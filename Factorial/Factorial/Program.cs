4c700560dea3ca36803d7437ead86d00e0aade6c
mmanalo <marksmanalo@marks-mbp-2.attlocal.net>
2018-05-10 21:13:18 -0700

    factorial

---
+++ Factorial/Factorial/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+// n! = n*(n-1)*(n-2)*...(1)
+namespace Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(Factorial(5));
+            Console.ReadLine();
+        }
+
+        private static int Factorial(int n)
+        {
+            var result = 1;
+            if (n == 0) return result;
+            else
+            {
+                for (var i = 2; i <= n; i++) 
+                {
+                    result = result * i;   
+                }    
+            }
+
+            return result;
+        }
+    }
+}
