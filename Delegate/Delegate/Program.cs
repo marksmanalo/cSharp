f41ec5a6b96b9da9b9da52e6f8ad5573ff8b5f21
mmanalo <marksmanalo@marks-mbp-2.attlocal.net>
2018-05-11 00:02:03 -0700

    Added delegate example

---
+++ Delegate/Delegate/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Delegate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDelegate myDelegate = Add;
+            Console.WriteLine(myDelegate(5, 6));
+
+            myDelegate = Multiply;
+            Console.WriteLine(myDelegate(7, 9));
+        }
+
+        // This delegate can take any function that
+        // returns an int and has two int inputs
+        public delegate int MyDelegate(int a, int b);
+
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
---
+++ ExtensionMethods/ExtensionMethods/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ExtensionMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int evenNumber = 6;
+            int oddNumber = 7;
+
+            Console.WriteLine(evenNumber.IsEven());
+            Console.WriteLine(oddNumber.IsEven());
+        }
+    }
+
+    public static class MyExtensionMethods
+    {
+        public static bool IsEven(this int input)
+        {
+            return input % 2 == 0;
+        }
+    }
+}
