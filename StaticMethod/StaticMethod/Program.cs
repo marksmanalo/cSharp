0f6d0e13f20b60e7119d8d378731d222315cd398
mmanalo <marksmanalo@marks-mbp-2.attlocal.net>
2018-05-10 23:16:35 -0700

    added static method example

---
+++ StaticMethod/StaticMethod/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace StaticMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var calculator = new Calculator();
+            var result1 = calculator.Add(1, 5);
+            var result2 = Calculator.Multiply(7, 4);
+
+            Console.WriteLine(result1);
+            Console.WriteLine(result2);
+        }
+    }
+
+    public class Calculator
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
