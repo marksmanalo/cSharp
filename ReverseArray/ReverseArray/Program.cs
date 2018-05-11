42cadcd98c4844078ea45b13a3ee3493286f8277
mmanalo <marksmanalo@marks-mbp-2.attlocal.net>
2018-05-10 22:00:27 -0700

    reverse array

---
+++ ReverseArray/ReverseArray/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ReverseArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var array = new int[] { 5, 4,3, 2, 1};
+            Console.WriteLine("Original: ");
+            foreach (var number in array){
+                Console.WriteLine(number);
+            }
+            var reversedArray = ReverseArray(array);
+            Console.WriteLine("Reversed: ");
+            foreach (var number in reversedArray)
+            {
+                Console.WriteLine(number);
+            }
+
+            Console.ReadLine();
+        }
+
+        private static int[] ReverseArray(int[] array)
+        {
+            var lastIndex = array.Length - 1;
+            var firstIndex = 0;
+            for (var i = 0; i < array.Length / 2; i++)
+            {
+                var holder = array[lastIndex];
+                array[lastIndex] = array[firstIndex];
+                array[firstIndex] = holder;
+                lastIndex -= 1;
+                firstIndex += 1;
+            }
+
+            return array;
+        }
+    }
+}
