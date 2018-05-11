d8b35af7e562c3ae8364ecb6960949e79d2fdff0
mmanalo <marksmanalo@marks-mbp-2.attlocal.net>
2018-05-10 23:11:34 -0700

    static class example

---
+++ StaticClass/StaticClass/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace StaticClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result1 = Calculator.Add(1, 5);
+            var result2 = Calculator.Multiply(4, 7);
+            Console.WriteLine(result1);
+            Console.WriteLine(result2);
+        }
+    }
+
+    static class Calculator
+    {
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
